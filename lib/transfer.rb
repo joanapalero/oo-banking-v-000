118b9f90b7ecc227643f25780bc47ecb2527600d
Joana Palero <joana.palero24@gmail.com>
2018-07-08 16:21:10 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,5 +12,24 @@ class Transfer
     @sender.valid? && @receiver.valid?
   end
   
+  def execute_transaction
+    if sender.valid? && receiver.valid? && status == "pending"
+      if amount < @sender.balance
+        @sender.balance -= amount
+        @receiver.balance += amount
+        @status = "complete"
+      else
+        @status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      end
+    end
+  end
   
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += amount
+      @receiver.balance -= amount
+      @status = "reversed"
+    end
+  end
 end
